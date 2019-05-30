410b4fc559e20b317c0d67d3bebdb172fed98942
Michał Sibik <chudymichal123@gmail.com>
2019-05-30 19:10:58 +0200

    jakies poprawki

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const request = require('request'); //modul ktory bedzie wykonywal zapytania
-var artist = 'BudkaSuflera'; //domyslna wartosc
+const readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    terminal: false
+});
+
+var artist = 'Budka Suflera'; //domyslna wartosc
 
 if(process.argv[2]){
         artist = process.argv[2];   //nazwa zespolu
@@ -13,6 +20,9 @@ function filterGroup(group){    //funkcja filtrujaca informacje o zespolach
         return filteredGroup;
 }
 
+
+var memberRepeat = 0;
+
 function memberRequest(id, name){   //funkcja pobierajaca informacje o konkretnych czlonkach zespolu
         return new Promise(function (resolve, reject){
                     console.log(`Pobieram informacje nt. członka ${name}...`);
@@ -23,12 +33,90 @@ function memberRequest(id, name){   //funkcja pobierajaca informacje o konkretny
                                 var filteredGroups = groups.map(filterGroup);   //dla kazdego zespolu z tablicy groups wykonywana funkcja filterGroup
                                 resolve(filteredGroups);
                         }
+                        else if(res.statusCode == 429){ //jesli w odpowiedzi otrzymamy kod too many requests
+                            memberRepeat++;
+                            var timeout = 10000;
+                            if(memberRepeat > 1){   //przy ponowym bledzie 429 wydluzamy czas oczekiwania
+                                timeout = memberRepeat * 10000;
+                            }
+                            var seconds = timeout/1000;
+                            console.log(`Zbyt wiele zapytań, czekam ${seconds} sekund...`);
+                            setTimeout(function(){resolve(memberRequest(id, name))}, timeout);    //czekamy i powtarzamy zapytanie
+                        }
                         else{
-                                reject(res.statusCode);
+                            reject(res.statusCode);
                         }
                 })
         })
 }
+
+
+function groupRequest(groupID){
+    console.log('Pobieram informacje nt. zespołu...');
+    request({url: `https://api.discogs.com/artists/${groupID}`, headers: {'User-Agent':'test/0.1'}}, async function(error,res,bod){ //zapytanie o dane zespolu o konkretnym id
+        if(!error && res.statusCode == 200){
+            var arr = JSON.parse(bod);
+            var members = arr.members;  //tablica z danymi o czlonkach zespolu
+            if(!members){
+                return console.log('Nie znaleziono zespołu');
+            }
+            var groupSet = new Set();   //zbior wszystkich zespolow do ktorych nalezeli czlonkowie
+            for(member of members){
+                    try{
+                            memberRepeat = 0;
+                            var groups = await memberRequest(member.id, member.name);   //zapytanie o dane kazdego czlonka zespolu
+                            member.groups = groups.map((group) => { //w obiekcie kazdego czlonka zapisujemy id wszystkich zespolow w jakich gral
+                                return group.id;
+                            });
+                    }catch(e){
+                            console.log(`${e}: Blad w trakcie memberRequest`);
+                            return e;
+                    }
+                    groups.forEach((group) => {
+                            if(!groupSet.has(JSON.stringify(group))){   //niestety w js porownywanie obiektow jest slabe (obiekty sa rowne sobie jesli maja ta sama referencje)
+                                groupSet.add(JSON.stringify(group));    //dlatego obiekty konwertujemy do JSON-owych stringow
+                            }
+                    })
+            }
+
+            var groupArrJSON = Array.from(groupSet);    //nastepnie konwertujemy zbior zespolow z powrotem do tablicy js-owych obiektow
+            var groupArr = groupArrJSON.map((groupJSON) => {
+                return JSON.parse(groupJSON);
+            })
+
+            for(group of groupArr){ //zbior wszystkich zespolow
+                group.members = []; //dla kazdego zespolu tworzymy nowe pole, czyli tablice zawierajaca wystepujacych tam muzykow
+                for(member of members){
+                    if(member.groups.includes(group.id)){   //jesli czlonek wystepowal w danym zespole,
+                        group.members.push(member);         //to jest wstawiany na liste czlonkow danego zespolu
+                    }
+                }
+            }
+
+            console.log('');
+            console.log('Inne zespoły, w których grało przynajmniej dwóch członków wyszukiwanego zespołu:')
+            console.log('');
+            for(group of groupArr){
+                if(group.members.length > 1 && group.id != groupID){    //interesuja nas tylko grupy w ktorych przynajmniej 2 czlonkow wyszukiwanego zespolu gralo razem i odrzucamy zespol podany jako argument programu
+                    console.log(`${group.name}:`);
+                    for(member of group.members){
+                        console.log(member.name);
+                    }
+                    console.log('');
+                }
+            }
+        }
+        else if(res.statusCode == 429){
+            console.log('Zbyt wiele zapytań, czekam 10 sekund...');
+            setTimeout(function(){groupRequest(groupID)}, 10000);
+        }
+        else{
+            console.log(`Błąd w trakcie wyszukiwania informacji nt. zespołu: ${res.statusCode}`);
+            return error;
+        }
+    });
+}
+
 
 console.log('');
 console.log('Wyszukuję id zespołu...');
@@ -37,68 +125,21 @@ request({url, headers: {'User-Agent':'test/0.1'}}, function(error,response,body)
                 if(JSON.parse(body).results.length == 0){
                         return console.log("Nie znaleziono zespolu, wpisz poprawna nazwe");
                 }
-                var groupID = JSON.parse(body).results[0].id;
-                console.log('Pobieram informacje nt. zespołu...');
-                request({url: `https://api.discogs.com/artists/${groupID}`, headers: {'User-Agent':'test/0.1'}}, async function(error,res,bod){ //zapytanie o dane zespolu o konkretnym id
-                    if(!error && res.statusCode == 200){
-                        var arr = JSON.parse(bod);
-                        var members = arr.members;  //tablica z danymi o czlonkach zespolu
-                        if(!members){
-                            return console.log("Nie znaleziono zespolu")
-                        }
-                        var groupSet = new Set();   //zbior wszystkich zespolow do ktorych nalezeli czlonkowie
-                        for(member of members){
-                                try{
-                                        var groups = await memberRequest(member.id, member.name);   //zapytanie o dane kazdego czlonka zespolu
-                                        member.groups = groups.map((group) => { //w obiekcie kazdego czlonka zapisujemy id wszystkich zespolow w jakich gral
-                                            return group.id;
-                                        });
-                                }catch(e){
-                                        console.log(`${e}: Blad w trakcie memberRequest`);
-                                        return e;
-                                }
-                                groups.forEach((group) => {
-                                        if(!groupSet.has(JSON.stringify(group))){   //niestety w js porownywanie obiektow jest slabe (obiekty sa rowne sobie jesli maja ta sama referencje)
-                                            groupSet.add(JSON.stringify(group));    //dlatego obiekty konwertujemy do JSON-owych stringow
-                                        }
-                                })
-                        }
-
-                        var groupArrJSON = Array.from(groupSet);    //nastepnie konwertujemy zbior zespolow z powrotem do tablicy js-owych obiektow
-                        var groupArr = groupArrJSON.map((groupJSON) => {
-                            return JSON.parse(groupJSON);
-                        })
-
-                        for(group of groupArr){ //zbior wszystkich zespolow
-                            group.members = []; //dla kazdego zespolu tworzymy nowe pole, czyli tablice zawierajaca wystepujacych tam muzykow
-                            for(member of members){
-                                if(member.groups.includes(group.id)){   //jesli czlonek wystepowal w danym zespole,
-                                    group.members.push(member);         //to jest wstawiany na liste czlonkow danego zespolu
-                                }
-                            }
-                        }
-
-                        console.log('');
-                        console.log('Inne zespoły, w których grało przynajmniej dwóch członków wyszukiwanego zespołu:')
-                        console.log('');
-                        for(group of groupArr){
-                            if(group.members.length > 1 && group.id != groupID){    //interesuja nas tylko grupy w ktorych przynajmniej 2 czlonkow wyszukiwanego zespolu gralo razem i odrzucamy zespol podany jako argument programu
-                                console.log(`${group.name}:`);
-                                for(member of group.members){
-                                    console.log(member.name);
-                                }
-                                console.log('');
-                            }
-                        }
+                var results = JSON.parse(body).results;
+                if(results[0].title.toUpperCase() != artist.toUpperCase()){
+                    console.log('');
+                    console.log('Który zespół wybrać?');
+                    for(i=0;i<3;i++){
+                        console.log(`${i+1}. ${results[i].title}`);
                     }
-                    else{
-                        console.log(`Błąd w trakcie wyszukiwania informacji nt. zespołu: ${error}`);
-                        return error;
-                    }
-                });
+                    console.log('');
+                    console.log('Podaj numer: ');
+                }
+                var groupID = results[0].id;
+                groupRequest(groupID);
         }
         else{
-            console.log(`Błąd w trakcie wyszukiwania id zespołu: ${error}`);
+            console.log(`Błąd w trakcie wyszukiwania id zespołu: ${response.statusCode}`);
             return error;
         }
 });
